6c9e304c0ce03f2013dcef0eae508382e6d2986a
Evan <evanmschultz@gmail.com>
2023-08-13 23:59:13 -0600

    Initial commit

---
+++ first_fastAPI/main.py
@@ -0,0 +1,78 @@
+from enum import Enum
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+
+"""
+Path parameters with typing
+"""
+
+
+@app.get("/items/{item_id}")
+async def read_item(item_id: int):
+    return {"item_id": item_id}
+
+
+"""
+Paths are read in order, so place fixed paths before variable paths.
+If not done this way the path would be interpreted as "/users/{user_id} = "me"
+instead of "/users/me".
+"""
+
+
+@app.get("/users/me")
+async def read_user_me():
+    return {"user_id": "the current user"}
+
+
+@app.get("/users/{user_id}")
+async def read_user(user_id: str):
+    return {"user_id": user_id}
+
+
+"""
+Enum (enumeration) example:
+Enums allow you to define a set of symbolic names bound to unique, constant values, 
+instead of vague generic types, ie. str.
+"""
+
+
+class ModelName(str, Enum):
+    alexnet = "alexnet"
+    resnet = "resnet"
+    lenet = "lenet"
+
+
+@app.get("/models/{model_name}")
+async def get_model(model_name: ModelName):
+    # Compare arg with the literal enumeration member
+    if model_name is ModelName.alexnet:
+        return {"model_name": model_name, "message": "Deep Learning FTW!"}
+
+    # Compare arg value with member value
+    if model_name.value == "lenet":
+        return {"model_name": model_name, "message": "LeCNN all the images"}
+
+    return {"model_name": model_name, "message": "Have some residuals"}
+
+
+"""
+Path parameters {file_path:path} the `path` indicates that the parameter must
+be a path.
+
+You could need the parameter to contain /home/john_doe/myfile.txt, with a leading slash (/).
+
+In that case, the URL would be: /files//home/john_doe/myfile.txt, with a double slash (//) 
+between files and home.
+"""
+
+
+@app.get("/files/{file_path:path}")
+async def read_file(file_path: str):
+    return {"file_path": file_path}
